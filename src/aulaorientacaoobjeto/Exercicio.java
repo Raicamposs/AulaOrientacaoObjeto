b5c7f24b9b376d46eea77ee8416ea700eaef4a1d
RAIANE CAMPOS JANUARIO <raianejanu@hotmail.com>
2015-03-14 20:50:21 -0300

    Projetos em sala
    Exercicios propostos pelo professor de O.O

---
+++ src/aulaorientacaoobjeto/Exercicio.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aulaorientacaoobjeto;
+
+import javax.swing.JOptionPane;
+import jdk.nashorn.internal.runtime.JSType;
+
+public class Exercicio {
+
+    public void BalancoTrimestral() {
+        int gastosJaneiro = 15000, gastosFevereiro = 23000, gastosMarco = 17000, mediaMensal;
+        int gastosTrimestrais = gastosJaneiro + gastosFevereiro + gastosMarco;
+
+        mediaMensal = (gastosTrimestrais / 3);
+        System.out.println(" Gastos Trimestrais = " + gastosTrimestrais + "\n Valor da Media Mensal = " + mediaMensal);
+
+    }
+
+    public void ExercicioNumero1() {
+
+        //1.Imprima todos os números de 120 a 530
+        for (int i = 120; i <= 530; i++) {
+            System.out.println(i);
+        }
+
+    }
+
+    public void ExercicioNumero2() {
+
+        //2.Imprima a soma de 1 até 1000
+        int soma = 0;
+        for (int i = 1; i <= 1000; i++) {
+            soma += i;
+        }
+        System.out.println(soma);
+    }
+
+    public void ExercicioNumero3() {
+
+        //3.Some todos os múltiplos de 3, entre 1 e 100
+        int soma = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                soma += i;
+            }
+
+        }
+        System.out.println(soma);
+    }
+
+    public void ExercicioNumero4() {
+
+        //4.Imprima os fatoriais de 1 a 10
+        int fatorial, aux;
+
+        for (int i = 1; i <= 10; i++) {
+            fatorial = 1;
+            aux = i;
+            while (aux > 1) {
+                fatorial = fatorial * (aux);
+                aux--;
+            }
+            System.out.println("O fatorial de " + i + " é: " + fatorial);
+        }
+    }
+
+    public void ExercicioNumero5(int x) {
+
+//5.Escreva um programa que, dada uma variável x com algum valor inteiro, temos um novo x de acordo com a seguinte regra:
+//•se x é par, x = x / 2
+//•se x é impar, x = 3 * x + 1
+//•imprime x
+//•O programa deve parar quando x tiver o valor final de 1.
+//•Exemplo para x = 13 , a saída será:
+//•40 -> 20 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1
+        System.out.println("Valor de x = " + x);
+        while (x > 1) {
+            if (x % 2 == 0) {
+                x /= 2;
+            } else {
+                x = ((3 * x) + 1);
+            }
+            System.out.print(" -> " + x);
+        }
+
+    }
+
+    public void ExercicioNumero6() {
+        aplicacaoCpf aplicacao = new aplicacaoCpf();
+        String cpf = aplicacao.cpfParseString();
+        String digitosFinais = "" + aplicacao.calculoDigito(aplicacao.dvCpf(10, cpf)) + aplicacao.calculoDigito(aplicacao.dvCpf(11, cpf));
+        if (cpf.endsWith(digitosFinais)) {
+            JOptionPane.showMessageDialog(null, "Cpf Valido!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Cpf Invalido!");
+        }
+
+    }
+
+}
---
+++ src/aulaorientacaoobjeto/aplicacaoCpf.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aulaorientacaoobjeto;
+
+import javax.swing.JOptionPane;
+import jdk.nashorn.internal.runtime.JSType;
+
+/**
+ *
+ * @author Raiane Campos
+ */
+public class aplicacaoCpf {
+
+    public String cpfParseString() {
+        String numCpf = null;
+        Boolean confere = false;
+        while (!confere == true) {
+
+            numCpf = JOptionPane.showInputDialog(null, "Qual Numero do Cpf?\nInsira apenas os digitos: ");
+            if (numCpf == null) {
+                System.exit(0);
+            } else {
+                numCpf = numCpf.trim();
+                if (JSType.isNumber(numCpf) && numCpf.length() == 11) {
+                    confere = true;
+                } else {
+                    JOptionPane.showMessageDialog(null, "Numero de Cpf esta com formato incorreto!");
+
+                }
+            }
+        }
+        return numCpf;
+    }
+
+    public int dvCpf(int num, String numCpf) {
+
+        int j = num;
+        int soma = 0;
+
+        for (int i = 0; i < (num - 1); i++) {
+            String digito = "" + numCpf.charAt(i);
+            int digitoCpf = Integer.parseInt(digito);
+            soma += (digitoCpf * j);
+            j--;
+        }
+
+        return soma;
+
+    }
+
+    public int calculoDigito(int sm) {
+        return 11 - (sm % 11);
+    }
+}
